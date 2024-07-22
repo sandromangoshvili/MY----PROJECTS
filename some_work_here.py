3a2ca99007d551641bd1304a301bb5a70c4429f6
sandromangoshvili <sandromangosvili@gmail.com>
2024-07-22 18:09:55 +0400

    first commit

---
+++ some_work_here.py
@@ -0,0 +1,10 @@
+name = ("sandro")
+surname = ("mangoshvili")
+age = ("12")
+text = "I am programmer"
+my_goal = ("become developer")
+who_is_helping_you = ("GOA")
+
+
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + "my_goal_is"  + " " + my_goal + " " + "who is helping you"  + " " + who_is_helping_you)
